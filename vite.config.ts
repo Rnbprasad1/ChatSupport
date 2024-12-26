6d47c3fa67a429d1c3f3d0b929de9c29e46fd709
Thirupathi Raju Vattem <tvattem@wayfair.com>
2024-12-26 12:02:21 +0530

    config ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path'; // Import path module
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // Define alias for '@' to point to 'src'
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
